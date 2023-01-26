83532e9d3615bacc3334e78d801458cad4d2377b
JaiK12 <101117168+JaiK12@users.noreply.github.com>
2023-01-26 09:19:17 +0530

    Update app.py

--- healthbot/app.py
+++ healthbot/app.py
@@ -3,19 +3,22 @@ from twilio.twiml.messaging_response import MessagingResponse
 from chatbot import ask, append_interaction_to_chat_log
 
 app = Flask(__name__)
-app.config['SECRET KEY'] =  '019phungalala694200'
-app.config['WTF_CSRF_ENABLED'] = False
-app.config['WTF_CSRF_SECRET_KEY'] = 'a random string'
 
-@app.route('/chatbot', methods=['GET'])
+app.secret_key = "019phungalala694200"
+app.config['SECRET KEY'] =  '019phungalala694200'
+
+
+@app.route('/chatbot', methods=['POST'])
+
 def bot():
-    incoming_msg = request.form.get('Body')
+    incoming_msg = request.values['BODY']
     chat_log = session.get('chat_log')
     answer = ask(incoming_msg, chat_log)
     session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer, chat_log)
     msg = MessagingResponse()
     msg.message(answer)
     return str(msg)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
