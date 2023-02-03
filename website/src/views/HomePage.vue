8c9736df00a4b1f6f5c4460215f81953feb5dba8
tpncoder <tejasprotim@gmail.com>
2023-02-03 17:32:18 +0530

    ui changes

--- website/src/views/HomePage.vue
+++ website/src/views/HomePage.vue
@@ -36,9 +36,13 @@
 
 <template>
   <main>
-		<h1 v-if="loading" class='text-center text-5xl mt-10'>loading...</h1>
-		<div v-if="!loading">
-  		<h1 class='relative left-10 text-5xl mt-11'>Hi!, {{ data.name }}</h1>
+		<div v-if="!store.signedIn">
+			
+		</div>
+
+		<h1 v-if="loading && store.signedIn" class='text-center text-5xl mt-10'>loading...</h1>
+		<div v-if="!loading && store.signedIn" id="user-card" class="bg-green-700 text-white pb-10 pt-2 mt-10 w-1/2 relative left-80 rounded-lg shadow-2xl">
+  		<h1 class='text-5xl mt-11 text-center'>Hi!, {{ data.name }}</h1>
   		<h1 v-if="data.disease !== null" class='text-center text-5xl mt-11'>You have {{ data.disease }}</h1>
 			
 			<div id="unregistered" class="text-center mt-20 bg-green-700 w-32 text-white p-3 rounded-md relative left-10" v-else>
